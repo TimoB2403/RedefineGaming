e1eb7a644e7e4c562e1ac0cadde82299b6357d7d
Timo Baumeister <TimoBaumeister2403@gmail.com>
2025-02-27 17:02:11 +0100

    AboutSectionAdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Hero from "./components/Hero";
+import About from "./components/About";
+import './index.css';
+
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
     </main>
   );
 }
